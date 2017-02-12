d94a95ac77529ab798caabf51b784c08d6bfa9bf
pjcevans <peterjcevans@gmail.com>
2017-02-12 01:48:36 +0000

    added tag previews

--- src/components/ui/SearchTool.js
+++ src/components/ui/SearchTool.js
@@ -8,13 +8,27 @@ class SearchTool extends Component {
   constructor(props) {
       super(props)
       this.state = {
-        search: ""
+        search: "",
+        typeSnack: this.filterByTag("recipesize", "snack"),
+        typeMain: this.filterByTag("recipesize", "main"),
+        typeSide: this.filterByTag("recipesize", "side")
       }
+  }
+
+  filterByTag(type, tag) {
+    // if filtering by just type (no tag provided) use [this code] instead
+    let filteredRecipes = myData.items.filter(
+      (recipe) => {
+        return recipe.tags[type].indexOf(tag) > -1
+      }
+    )
+    return filteredRecipes
   }
 
   updateSearch(event) {
     this.setState({
       search: event.target.value.substr(0,20)
+
     });
   }
 
@@ -28,21 +42,24 @@ class SearchTool extends Component {
 
     return (
     <div id="searchbox">
+
       <div id="filterpane">
         <p>Filter your results:</p>
         <p>Type of meal</p>
         <ul>
-          <li>Snack (4)</li>
-          <li>Main (16)</li>
-          <li>Side (24)</li>
+          <li>Snack ({this.state.typeSnack.length})</li>
+          <li>Main ({this.state.typeMain.length})</li>
+          <li>Side ({this.state.typeSide.length})</li>
         </ul>
       </div>
+
       <div>
         <input type="text"
                value={this.state.search}
                onChange={this.updateSearch.bind(this)}/>
         <RecipeList recipes={filteredRecipes}/>
       </div>
+
     </div>
   )
  }
