d253f13a661052db152340af1723dc2568303ee9
pjcevans <peterjcevans@gmail.com>
2017-03-05 14:19:08 +0000

    updating styling of homepage nav chevrons

--- src/components/ui/HomeShowRecipe.js
+++ src/components/ui/HomeShowRecipe.js
@@ -1,6 +1,9 @@
+import FaDown from 'react-icons/lib/fa/chevron-down'
+
+
 const HomeShowRecipe = (props) =>
-  <div className="show-next-item-button">
-    <button onClick={() => props.showAnotherRecipe()}>Show next recipe...</button>
+  <div className="show-next-block">
+    <button className="show-next-button" onClick={() => props.showAnotherRecipe()}>Show next recipe...<br /> <FaDown /></button>
   </div>
 
 export default HomeShowRecipe
